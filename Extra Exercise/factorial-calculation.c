bd1c905d42c337b4eb8b0342f01300b5c3b0e4c3
Deepak Agarwal <154506573+DeepakAgarwal7405@users.noreply.github.com>
2026-02-12 23:12:50 +0530

    Update factorial-calculation.c

--- Extra Exercise/factorial-calculation.c
+++ Extra Exercise/factorial-calculation.c
@@ -1,36 +1,41 @@
 #include <stdio.h>
-#include <time.h>
 
-unsigned long long factorial_iterative(int n) {
-    unsigned long long fact = 1;
-    int i;
-    for (i = 1; i <= n; i++) fact *= i;
+int factorial(int n) {
+  
+    // Base Case:
+    if (n == 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+
+int loopfactorial(int num) {
+    int fact = 1, i;
+
+    
+    // Loop from 1 to N to get the factorial
+    for (i = 1; i <= num; i++) {
+        fact *= i;
+    }
+
     return fact;
 }
 
-unsigned long long factorial_recursive(int n) {
-    if (n == 0 || n == 1) return 1ULL;
-    return (unsigned long long)n * factorial_recursive(n - 1);
+int main() {
+    int num;
+
+    printf("Enter the Positive number:- ");
+    scanf("%d",&num);
+
+     if (num < 0) {
+        printf("Error! Factorial of a negative number doesn't exist.\\n");
+    } 
+    else {
+            int fact = loopfactorial(num);
+            printf("loopFactorial of %d is %d", num, fact);
+
+            printf("\n Factorial of %d is %d", num, factorial(num));
+        }
+    return 0;
 }
-
-main() {
-    int n;
-    clock_t start, end;
-    double sec;
-
-    printf("Enter a number (<=20 recommended): ");
-    scanf("%d", &n);
-
-    start = clock();
-    printf("Factorial (Iterative) = %llu\n", factorial_iterative(n));
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Time taken by Iterative = %f seconds\n", sec);
-
-    start = clock();
-    printf("Factorial (Recursive) = %llu\n", factorial_recursive(n));
-    end = clock();
-    sec = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("Time taken by Recursive = %f seconds\n", sec);
-}
-
